2044556cd289d9b2709f9a09237d9b0f864909e5
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-07-04 00:10:34 +0700

    Add buildSrc package

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,15 @@
+object Dependencies {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.3.72"
+}
+
+object Android {
+    const val ktx = "androidx.core:core-ktx:1.3.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+}
+
+object Testing {
+    const val jUnit = "junit:junit:4.13"
+    const val extJUnit = "androidx.test.ext:junit:1.1.1"
+    const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
+}
